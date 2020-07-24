d2571270358f61bcf2eeee0cb34220e41ea380a9
DanielAbioye <apsabioyed@gmail.com>
2020-07-24 13:38:00 +0100

    String variable Java Script

---
+++ income.js
@@ -0,0 +1,35 @@
+var name = "Daniel Abioye"
+
+var age = 28
+
+var annualPay = 100,000
+
+console.log(name)
+
+console.log(age)
+
+console.log(annualPay)
+
+
+
+document.write ("My name is" + name + "my age is" age + "and I hope to earn" + annualPay + "per year")
+
+
+
+var companysPercentage = 0.62;
+
+var salesForTheYear = 4,600,000;
+
+var totalMade = salesForTheYear * companysPercentage
+
+console.log(totalMade)
+
+
+
+var oneAcreOfLand = 43,560;
+
+var acreOfLand = 389,767;
+
+var total = acreOfLand / oneAcreOfLand
+
+console.log(total)
---
+++ personal-info.js
@@ -0,0 +1,18 @@
+var myName = "Daniel Abioye"
+
+var myAge = 28
+
+var myAddress = "27 Brewster road, leyton, london. E10 6RG"
+
+var myCollegeMajor = "University of bedfordshire"
+
+console.log(myName)
+
+console.log(myAge)
+
+console.log(myAddress)
+
+console.log(myCollegeMajor)
+
+
+
